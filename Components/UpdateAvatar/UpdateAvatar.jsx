b812b13db094e7e2cfe3355c073b4f6b865dc855
utsavgarchar63 <utsavgarchar63@gmail.com>
2024-07-22 16:22:51 +0530

    adsa

--- Components/UpdateAvatar/UpdateAvatar.jsx
+++ Components/UpdateAvatar/UpdateAvatar.jsx
@@ -1,32 +1,24 @@
-import { useRouter } from "next/router";
-import UpdateAvatarForm from "./UpdateAvatarForm";
-import { Container, Row, Col } from "react-bootstrap";
 
+import UpdateAvatarForm from "./UpdateAvatarForm";
 export default function UpdateAvatar() {
-  const router = useRouter();
-  const { email } = router.query;
   return (
     <>
       <svg
         className="wave"
+        style={{ zIndex: "-1" }}
         xmlns="http://www.w3.org/2000/svg"
         viewBox="0 0 1440 320"
       >
         <path
           fill="#5000ca"
           fillOpacity="1"
-          d="M0,128L48,122.7C96,117,192,107,288,133.3C384,160,480,224,576,218.7C672,213,768,139,864,128C960,117,1056,171,1152,186.7C1248,203,1344,181,1392,170.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
+          d="M0,128L48,117.3C96,107,192,85,288,74.7C384,64,480,64,576,85.3C672,107,768,149,864,160C960,171,1056,149,1152,144C1248,139,1344,149,1392,154.7L1440,160L1440,320L1392,320C1344,320,1248,320,1152,320C1056,320,960,320,864,320C768,320,672,320,576,320C480,320,384,320,288,320C192,320,96,320,48,320L0,320Z"
         ></path>
       </svg>
-      <Container fluid>
-        <Row>
-          <div className="d-flex align-items-center justify-content-center vh-100">
-            <Col xs={11} sm={8} md={6} lg={3}>
-              <UpdateAvatarForm />
-            </Col>
-          </div>
-        </Row>
-      </Container>
+
+      <div className="d-flex align-items-center justify-content-center vh-100">
+        <UpdateAvatarForm />
+      </div>
     </>
   );
 }
--- Components/UpdateAvatar/UpdateAvatarForm.jsx
+++ Components/UpdateAvatar/UpdateAvatarForm.jsx
@@ -1,23 +1,41 @@
 import React, { useState, useEffect } from "react";
 import { useRouter } from "next/router";
-import Image from "next/image";
-import { Skeleton } from "@nextui-org/react";
-import { ApiGet } from "../../Helper/APIData";
+import Skeleton, { SkeletonTheme } from "react-loading-skeleton";
+import { Container, Row, Col } from "react-bootstrap";
+import "react-loading-skeleton/dist/skeleton.css";
+import { toast } from "react-hot-toast";
+import { ApiGet, ApiPut } from "../../Helper/APIData";
 
-const boyId = [
-  40, 16, 33, 2, 24, 34, 17, 32, 15, 35, 47, 7, 30, 9, 45, 23, 14, 13, 3, 21, 6,
-  31, 22, 43, 50, 12, 25, 11, 10, 39, 8, 19, 49, 27, 18, 37, 36, 44, 29, 48, 26,
-  41, 5, 1, 4, 42, 20, 38,
-];
-const girlId = [
-  95, 61, 74, 51, 81, 78, 53, 84, 94, 72, 75, 77, 67, 82, 90, 65, 71, 80, 57,
-  55, 99, 83, 59, 58, 73, 69, 76, 64, 52, 66, 88, 91, 92, 63, 86, 93, 98, 100,
-  87, 70, 62, 97, 60, 96, 85, 56, 79, 54, 89,
-];
+const avatarIds = {
+  male: [
+    40, 16, 33, 2, 24, 34, 17, 32, 15, 35, 47, 7, 30, 9, 45, 23, 14, 13, 3, 21,
+    6, 31, 22, 43, 50, 12, 25, 11, 10, 39, 8, 19, 49, 27, 18, 37, 36, 44, 29,
+    48, 26, 41, 5, 1, 4, 42, 20, 38,
+  ],
+  female: [
+    95, 61, 74, 51, 81, 78, 53, 84, 94, 72, 75, 77, 67, 82, 90, 65, 71, 80, 57,
+    55, 99, 83, 59, 58, 73, 69, 76, 64, 52, 66, 88, 91, 92, 63, 86, 93, 98, 100,
+    87, 70, 62, 97, 60, 96, 85, 56, 79, 54, 89,
+  ],
+};
+
+const getRandomIds = (array, count = 4) => {
+  const uniqueIndexes = new Set();
+  const tempArray = [...array];
+
+  while (uniqueIndexes.size < count && tempArray.length > 0) {
+    const randomIndex = Math.floor(Math.random() * tempArray.length);
+    uniqueIndexes.add(tempArray[randomIndex]);
+    tempArray.splice(randomIndex, 1);
+  }
+
+  return Array.from(uniqueIndexes);
+};
 
 export default function UpdateAvatarForm() {
-  const [gender, setGenderData] = useState("");
+  const [loading, setLoading] = useState(true);
   const [avatarUrls, setAvatarUrls] = useState([]);
+  const [selectedAvatar, setSelectedAvatar] = useState(null);
   const router = useRouter();
   const { query } = router;
 
@@ -30,10 +48,10 @@ export default function UpdateAvatarForm() {
   const fetchUserData = async (id) => {
     try {
       const response = await ApiGet(`/user/get-user?id=${id}`);
-      if (response?.data?.success === true) {
-        const userGender = response?.data?.data?.vGender;
-        setGenderData(userGender);
-        fetchAvatars(userGender);
+      if (response?.data?.success) {
+        const userGender = response.data.data.vGender;
+        const genderKey = userGender === "male" ? "male" : "female";
+        fetchAvatars(genderKey);
       } else {
         console.error(`Failed to fetch user data: ${response.status}`);
       }
@@ -42,60 +60,131 @@ export default function UpdateAvatarForm() {
     }
   };
 
-  const fetchAvatars = async (gender) => {
-    const ids = gender === "male" ? getRandomIds(boyId) : getRandomIds(girlId);
+  const fetchAvatars = (gender) => {
+    const ids = getRandomIds(avatarIds[gender]);
     const urls = ids.map((id) => `https://avatar.iran.liara.run/public/${id}`);
-    setAvatarUrls(urls);
+    setTimeout(() => {
+      setAvatarUrls(urls);
+      setLoading(false);
+    }, 1000);
+  };
+  const handleAvatarClick = (url) => {
+    setSelectedAvatar(url);
   };
 
-  const getRandomIds = (array) => {
-    let result = [];
-    const tempArray = [...array];
-    while (result.length < 4 && tempArray.length) {
-      const randomIndex = Math.floor(Math.random() * tempArray.length);
-      result.push(tempArray.splice(randomIndex, 1)[0]);
+  const handleUpdateAvatar = async () => {
+    if (selectedAvatar) {
+      try {
+        const response = await ApiPut(`/user/update-user-avatar`, {
+          avatarPath: selectedAvatar,
+        });
+        console.log(response?.data)
+        if (response?.data?.success) {
+          toast.success(res.data?.message, {
+            duration: 4000,
+            position: "top-right",
+            icon: "📧",
+          });
+        } else {
+          console.error(`Failed to update avatar: ${response.status}`);
+        }
+      } catch (error) {
+        console.error("Error updating avatar:", error);
+      }
+    } else {
+      alert("Please select an avatar.");
     }
-    return result;
   };
-
   return (
-    <>
-      <div style={{ display: "flex", flexWrap: "wrap", gap: "10px" }}>
-        {avatarUrls.length === 0
-          ? [1, 2, 3, 4].map((_, index) => (
-              <div
-                key={index}
-                style={{
-                  position: "relative",
-                  width: "100px",
-                  height: "100px",
-                }}
-              >
-                <Skeleton
-                  css={{ width: "100%", height: "100%", borderRadius: "8px" }}
-                />
-              </div>
-            ))
-          : avatarUrls.map((url, index) => (
-              <div
-                key={index}
-                style={{
-                  position: "relative",
-                  width: "100px",
-                  height: "100px",
-                }}
-              >
-                <Image
-                  src={url}
-                  alt={`Avatar ${index}`}
-                  layout="fill"
-                  objectFit="cover"
-                  placeholder="blur"
-                  blurDataURL={url} // Use a low-quality image placeholder or a tiny version of the image
-                />
-              </div>
-            ))}
+    <Container fluid>
+      <Row className="justify-content-center">
+        <SkeletonTheme baseColor="#202020" highlightColor="#444">
+          {loading
+            ? Array.from({ length: 4 }).map((_, index) => (
+                <Col
+                  key={index}
+                  xs={10}
+                  sm={4}
+                  md={3}
+                  lg={2}
+                  className="d-flex justify-content-center mb-4"
+                >
+                  <Skeleton
+                    circle
+                    height={200}
+                    width={200}
+                    className="skeleton-loader"
+                  />
+                </Col>
+              ))
+            : avatarUrls.map((url, index) => (
+                <Col
+                  key={index}
+                  xs={10}
+                  sm={4}
+                  md={3}
+                  lg={2}
+                  className="d-flex justify-content-center mb-4"
+                >
+                  <img
+                    src={url}
+                    alt={`Avatar ${index}`}
+                    className="avatar-img"
+                  />
+                </Col>
+              ))}
+        </SkeletonTheme>
+        <style jsx>{`
+          .skeleton-loader {
+            border-radius: 50%;
+            width: 200px;
+            height: 200px;
+            animation: skeleton-loading 1.5s infinite ease-in-out;
+          }
+
+          .avatar-img {
+            width: 200px;
+            height: 200px;
+            border-radius: 50%;
+            box-shadow: 0 4px 8px rgba(0, 0, 0, 0.2);
+          }
+
+          @keyframes skeleton-loading {
+            0% {
+              background-position: -200% 0;
+            }
+            100% {
+              background-position: 200% 0;
+            }
+          }
+
+          @media (max-width: 576px) {
+            .avatar-img {
+              width: 120px;
+              height: 120px;
+            }
+            .skeleton-loader {
+              width: 120px !important;
+              height: 120px !important;
+            }
+          }
+
+          @media (max-width: 360px) {
+            .avatar-img {
+              width: 100px;
+              height: 100px;
+            }
+
+            .skeleton-loader {
+              width: 100px !important;
+              height: 100px !important;
+            }
+          }
+        `}</style>
+      </Row>
+      <div className="mt-4 d-flex justify-content-center">
+        <button className="button">Update Avatar</button>
       </div>
-    </>
+    </Container>
   );
 }
